9e55e252cca9b1b93e57e0c3c1c39e126ed6110d
cuttovidhan <cuttovidhan@gmail.com>
2026-03-31 18:12:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("CUTTOVIDHAN Website Loaded");
+const video = document.getElementById("portfolioVideo");
+
+// Pause video when out of view
+window.addEventListener("scroll", () => {
+  const rect = video.getBoundingClientRect();
+
+  if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+    // visible → do nothing
+  } else {
+    video.pause();
+  }
+});
